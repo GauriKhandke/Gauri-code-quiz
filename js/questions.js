10f19a3bbbdb8c0207762652457cbe50cafaa3b5
Gauri Khandke <khandkegauri@gmail.com>
2020-06-29 15:06:37 -0700

    Created files and folder structure

---
+++ js/questions.js
@@ -0,0 +1,13 @@
+// Questions as an array of Objects
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts'
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses'
+    }
+  ];
